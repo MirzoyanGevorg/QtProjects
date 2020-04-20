75679739f504cc6c6d30eb0b84686a2c870a817f
Gevorg Mirzoyan <geva.mirzoyan1994@gmail.com>
2020-04-20 16:21:03 +0400

    Added Autherization window realization

--- RegistrationForm/authwindow.cpp
+++ RegistrationForm/authwindow.cpp
@@ -1,14 +1,33 @@
 #include "authwindow.h"
 #include "ui_authwindow.h"
 
-AuthWindow::AuthWindow(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::AuthWindow)
+AuthWindow::AuthWindow(QWidget *parent)
+    :   QWidget(parent)
+    ,   ui(new Ui::AuthWindow)
 {
     ui->setupUi(this);
 }
 
-AuthWindow::~AuthWindow()
-{
+AuthWindow::~AuthWindow() {
     delete ui;
+}
+
+void AuthWindow::on_lineEditName_textEdited(const QString& name)
+{
+    m_userName = name;
+}
+
+void AuthWindow::on_lineEditPasword_textEdited(const QString& pass)
+{
+    m_userPass = pass;
+}
+
+void AuthWindow::on_pushButtonLogin_clicked()
+{
+    emit login_button_clicked();
+}
+
+void AuthWindow::on_pushButtonRegister_clicked()
+{
+    emit register_button_clicked();
 }
--- RegistrationForm/authwindow.h
+++ RegistrationForm/authwindow.h
@@ -14,9 +14,23 @@ class AuthWindow : public QWidget
 public:
     explicit AuthWindow(QWidget *parent = 0);
     ~AuthWindow();
+    QString getLogin() const;
+    QString getPass() const;
+
+signals:
+    void login_button_clicked();
+    void register_button_clicked();
+
+private slots:
+    void on_lineEditName_textEdited(const QString& name);
+    void on_lineEditPasword_textEdited(const QString& pass);
+    void on_pushButtonLogin_clicked();
+    void on_pushButtonRegister_clicked();
 
 private:
     Ui::AuthWindow *ui;
+    QString m_userName;
+    QString m_userPass;
 };
 
 #endif // AUTHWINDOW_H

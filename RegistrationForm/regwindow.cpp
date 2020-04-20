ad18e88a7c38cf5308378bdd14b599d0718507ce
Gevorg Mirzoyan <geva.mirzoyan1994@gmail.com>
2020-04-20 17:08:54 +0400

    Registration window realization

--- RegistrationForm/regwindow.cpp
+++ RegistrationForm/regwindow.cpp
@@ -12,3 +12,38 @@ RegWindow::~RegWindow()
 {
     delete ui;
 }
+
+QString RegWindow::getName() const
+{
+    return m_userName;
+}
+
+QString RegWindow::getPass() const
+{
+    return m_userPass;
+}
+
+bool RegWindow::checkPass() const
+{
+    return m_confirm == m_userPass;
+}
+
+void RegWindow::on_lineEditName_textEdited(const QString& name)
+{
+    m_userName = name;
+}
+
+void RegWindow::on_lineEditPass_textEdited(const QString& pass)
+{
+    m_userPass = pass;
+}
+
+void RegWindow::on_lineEditConfirm_textEdited(const QString& confirm)
+{
+    m_confirm = confirm;
+}
+
+void RegWindow::on_pushButton_clicked()
+{
+    emit register_button_clicked();
+}
--- RegistrationForm/regwindow.h
+++ RegistrationForm/regwindow.h
@@ -14,9 +14,24 @@ class RegWindow : public QWidget
 public:
     explicit RegWindow(QWidget *parent = 0);
     ~RegWindow();
+    QString getName() const;
+    QString getPass() const;
+    bool checkPass() const;
+
+signals:
+    void register_button_clicked();
+
+private slots:
+    void on_lineEditName_textEdited(const QString& name);
+    void on_lineEditPass_textEdited(const QString& pass);
+    void on_lineEditConfirm_textEdited(const QString& confirm);
+    void on_pushButton_clicked();
 
 private:
     Ui::RegWindow *ui;
+    QString m_userName;
+    QString m_userPass;
+    QString m_confirm;
 };
 
 #endif // REGWINDOW_H

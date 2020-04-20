b32320abab2fd321a6a4306909cb96de65b2010b
Gevorg Mirzoyan <geva.mirzoyan1994@gmail.com>
2020-04-20 17:33:46 +0400

    Main Window class definition

--- RegistrationForm/mainwindow.h
+++ RegistrationForm/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QtSql/QtSql>
+#include "authwindow.h"
+#include "regwindow.h"
 
 namespace Ui {
     class MainWindow;
@@ -14,9 +18,28 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void display();
+    void connectDB();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* m_ui;
+
+    AuthWindow m_authUi;
+    RegWindow m_regUi;
+
+    QString m_userName;
+    QString m_userPass;
+    QString m_dbInput;
+
+    QSqlDatabase m_db;
+
+    int m_userCounter;
+    bool m_loginSucces;
+
+private slots:
+    void registerUser();
+    void authorizeUser();
+    void registerUserShow();
 };
 
 #endif // MAINWINDOW_H
